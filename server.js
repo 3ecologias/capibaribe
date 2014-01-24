73eb0994666978ceec14ed24571027a5a0a53041
Ricardo Brazileiro <rbrazileiro@gmail.com>
2014-01-24 20:26:19 -0300

    tumblr modal

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ app.get('/tumblr', function (req, res) {
   res.sendfile(__dirname + '/blog.html');
 });
 
+app.get('/tumblr-api', function (req, res) {
+  res.sendfile(__dirname + '/assets/js/jquery.tumblr-kit.min.js');
+});
+
 app.get('/*.(js|css|png|jpg)', function(req, res){
   res.sendfile(__dirname + '/assets'+req.url);
 });
