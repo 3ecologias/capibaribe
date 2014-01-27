baf550fd5dc099c29f2ea50bca211ede01438c86
Ricardo Brazileiro <rbrazileiro@gmail.com>
2014-01-27 18:08:17 -0300

    ajax

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ app.get('/tumblr', function (req, res) {
 //   res.sendfile(__dirname + '/blog.html');
 // });
 
-// app.get('/tumblr-api', function (req, res) {
-//   res.sendfile(__dirname + '/assets/js/jquery.tumblr-kit.min.js');
-// });
+app.get('/tumblr-api', function (req, res) {
+  res.sendfile(__dirname + '/assets/js/jquery.tumblr-kit.min.js');
+});
 
 app.get('/*.(js|css|png|jpg)', function(req, res){
   res.sendfile(__dirname + '/assets'+req.url);
