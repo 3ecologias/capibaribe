ff1d5f119becd7b9ebc92f1fe46d68d9ca6b9d52
Ricardo Brazileiro <rbrazileiro@gmail.com>
2014-01-27 19:33:32 -0300

    div load blog

--- assets/js/capibaribe.js
+++ assets/js/capibaribe.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
 	$( "#blog" ).click(function() {
 	  $( "#blog-content" ).slideToggle( "slow" );
-	});	
+	});
+
+    $('.nav li a').on('click', function(e){
+
+        e.preventDefault(); // prevent link click if necessary?
+
+        var $thisLi = $(this).parent('li');
+        var $ul = $thisLi.parent('ul');
+
+        if (!$thisLi.hasClass('active'));
+        {
+            $ul.find('li.active').removeClass('active');
+                $thisLi.addClass('active');
+        }
+
+    });	
 });
