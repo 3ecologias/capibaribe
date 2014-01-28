0fd838d1a0d024d41280c76a52380a92b4b76d39
rbrz <rbrazileiro@gmail.com>
2014-01-28 12:05:13 -0300

    come back to old position

--- assets/js/capibaribe.js
+++ assets/js/capibaribe.js
@@ -1,28 +1,28 @@
 $(document).ready(function() {
 
-	// $( "#blog, .close-blog" ).click(function() {
-	//  	$( "#blog-content" ).slideToggle( "slow" );
+	$( "#blog, .close-blog" ).click(function() {
+	 	$( "#blog-content" ).slideToggle( "slow" );
 
-	// 	if($("#blog-content").is(":hidden")) {
+		if($("#blog-content").is(":hidden")) {
 
-	// 	}
-	// });
+		}
+	});
 
-	// $( "#instagram, .close-insta" ).click(function() {
-	//  	$( "#instagram-content" ).slideToggle( "slow" );
+	$( "#instagram, .close-insta" ).click(function() {
+	 	$( "#instagram-content" ).slideToggle( "slow" );
 
-	// 	if($("#instafeed").is(":hidden")) {
+		if($("#instafeed").is(":hidden")) {
 			
-	// 	}
-	// });
+		}
+	});
 
-	// $( "#twitter, .close-twitter" ).click(function() {
-	//  	$( "#twitter-content" ).slideToggle( "slow" );
+	$( "#twitter, .close-twitter" ).click(function() {
+	 	$( "#twitter-content" ).slideToggle( "slow" );
 
-	// 	if($("#instafeed").is(":hidden")) {
+		if($("#instafeed").is(":hidden")) {
 			
-	// 	}
-	// });
+		}
+	});
 
     $('.nav li a').on('click', function(e){
 
@@ -30,7 +30,7 @@ $(document).ready(function() {
 
         var $thisLi = $(this).parent('li');
         var $ul = $thisLi.parent('ul');
-        var $id = $thisLi.find('a').attr('id');
+        // var $id = $thisLi.find('a').attr('id');
 
         if (!$thisLi.hasClass('active'));
         {
@@ -38,13 +38,13 @@ $(document).ready(function() {
                 $thisLi.addClass('active');
         }
 
-        if ($id == 'blog') {
-        	$( "#blog-content" ).slideToggle( "slow" );
-        } else if ($id == 'instagram') {
-        	$( "#instagram-content" ).slideToggle( "slow" );
-        } else if ($id == 'twitter') {
-        	$( "#twitter-content" ).slideToggle( "slow" );
-        }
+        // if ($id == 'blog') {
+        // 	$( "#blog-content" ).slideToggle( "slow" );
+        // } else if ($id == 'instagram') {
+        // 	$( "#instagram-content" ).slideToggle( "slow" );
+        // } else if ($id == 'twitter') {
+        // 	$( "#twitter-content" ).slideToggle( "slow" );
+        // }
 
         // if ($thisLi.hasClass('active')) {
         // 	$thisLi.click(function() {
