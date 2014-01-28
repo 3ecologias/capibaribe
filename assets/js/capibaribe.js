2623a30d23ef015c218e5b9c3861496730dfe655
rbrz <rbrazileiro@gmail.com>
2014-01-28 15:01:34 -0300

    test javascript

--- assets/js/capibaribe.js
+++ assets/js/capibaribe.js
@@ -1,6 +1,30 @@
 $(document).ready(function() {
 
-	$( "#blog, .close-blog" ).click(function() {
+	// $( "#blog, .close-blog" ).click(function() {
+	//  	$( "#blog-content" ).slideToggle( "slow" );
+
+	// 	if($("#blog-content").is(":hidden")) {
+
+	// 	}
+	// });
+
+	// $( "#instagram, .close-insta" ).click(function() {
+	//  	$( "#instagram-content" ).slideToggle( "slow" );
+
+	// 	if($("#instafeed").is(":hidden")) {
+			
+	// 	}
+	// });
+
+	// $( "#twitter, .close-twitter" ).click(function() {
+	//  	$( "#twitter-content" ).slideToggle( "slow" );
+
+	// 	if($("#instafeed").is(":hidden")) {
+			
+	// 	}
+	// });
+
+	$( ".close-blog" ).click(function() {
 	 	$( "#blog-content" ).slideToggle( "slow" );
 
 		if($("#blog-content").is(":hidden")) {
@@ -8,7 +32,7 @@ $(document).ready(function() {
 		}
 	});
 
-	$( "#instagram, .close-insta" ).click(function() {
+	$( ".close-insta" ).click(function() {
 	 	$( "#instagram-content" ).slideToggle( "slow" );
 
 		if($("#instafeed").is(":hidden")) {
@@ -16,7 +40,7 @@ $(document).ready(function() {
 		}
 	});
 
-	$( "#twitter, .close-twitter" ).click(function() {
+	$( ".close-twitter" ).click(function() {
 	 	$( "#twitter-content" ).slideToggle( "slow" );
 
 		if($("#instafeed").is(":hidden")) {
@@ -38,13 +62,13 @@ $(document).ready(function() {
                 $thisLi.addClass('active');
         }
 
-        // if ($id == 'blog') {
-        // 	$( "#blog-content" ).slideToggle( "slow" );
-        // } else if ($id == 'instagram') {
-        // 	$( "#instagram-content" ).slideToggle( "slow" );
-        // } else if ($id == 'twitter') {
-        // 	$( "#twitter-content" ).slideToggle( "slow" );
-        // }
+        if ($id == 'blog') {
+        	$( "#blog-content" ).slideToggle( "slow" );
+        } else if ($id == 'instagram') {
+        	$( "#instagram-content" ).slideToggle( "slow" );
+        } else if ($id == 'twitter') {
+        	$( "#twitter-content" ).slideToggle( "slow" );
+        }
 
         // if ($thisLi.hasClass('active')) {
         // 	$thisLi.click(function() {
