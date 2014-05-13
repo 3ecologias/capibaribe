ad20a4307f8ec315db8db1a6f570772827ca7f81
rbrz <rbrazileiro@gmail.com>
2014-05-13 10:53:06 -0300

    testing js carousel

--- assets/js/capibaribe.js
+++ assets/js/capibaribe.js
@@ -175,9 +175,16 @@ $(document).ready(function() {
 		closeEffect	: 'none'
 	});
 
-	// $('.carousel').carousel({
- // 		interval: 2000
- //  	});
+    $('.carousel').carousel({
+    	interval: 1000
+    });
+	$('a[data-slide="prev"]').click(function() {
+	  $('.carousel').carousel('prev');
+	});
+
+	$('a[data-slide="next"]').click(function() {
+	  $('.carousel').carousel('next');
+	});
 
 	// Menu Handlers
     // http://stackoverflow.com/questions/17059515/jquery-hover-with-animate-then-animate-left-in-second-handler-issue
