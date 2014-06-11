0fe233a1281b2649547819e8779cab902f5cbb09
rbrz <rbrazileiro@gmail.com>
2014-06-11 20:51:11 -0300

    remove touchstart

--- assets/js/capibaribe.js
+++ assets/js/capibaribe.js
@@ -25,7 +25,7 @@ $(document).ready(function() {
 	// });
 
 	//target the entire page, and listen for touch events
-	$('html, body').on('touchstart touchmove', function(e){ 
+	$('html, body').on('touchmove', function(e){ 
 	     //prevent native touch activity like scrolling
 	     e.preventDefault(); 
 	});
